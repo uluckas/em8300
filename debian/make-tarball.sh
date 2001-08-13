28048d55b45ca4d7add126d8660eb8a77ffcaa2b
Rick Haines <rick@kuroyi.net>
2001-08-13 19:50:54 +0200

    * debian/*: 	  updates

--- debian/make-tarball.sh
+++ debian/make-tarball.sh
@@ -4,7 +4,7 @@ PWD=`pwd`
 
 if [ ! -f debian/em8300.tar.gz ]; then
     cd /usr/src
-    tar cvzf em8300.tar.gz modules/em8300/
-    mv em8300.tar.gz modules/em8300/debian/em8300.tar.gz
+    tar cvzf em8300.tar.gz modules/em8300-0.8.1pre1/
+    mv em8300.tar.gz modules/em8300-0.8.1pre1/debian/em8300.tar.gz
     cd $PWD
 fi

819f47d99bc654d2f58a1c89101bf7f50e477338
Rick Haines <rick@kuroyi.net>
2001-09-02 22:49:50 +0200

    * debian/* 	big updates from Jonas and Graeme
    	* modules/Makefile:
    	new Makefile from Jonas/Graeme to
    	work with both 2.2 and 2.4 kernels

--- debian/setvers.sh
+++ debian/setvers.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+KVERS=`uname -r`
+
 echo "$KVERS" > debian/KVERS
 
 rm -f debian/control.tmp
-rm -f debian/control
 
-cat debian/source.control > debian/control.tmp
-sed -e 's/\${kvers}/'"$KVERS"'/g' debian/em8300-modules.control | \
-  tee -a debian/control >> debian/control.tmp
+sed -e 's/\${kvers}/'"$KVERS"'/g' debian/em8300-modules.control > \
+  debian/control.tmp
 
 dpkg-gencontrol -p"em8300-modules-$KVERS" -Pdebian/tmp-modules \
   -cdebian/control.tmp

1ed85936bc68e104753a9d1babc4673391391459
Jonas Birme <jonas@birme.se>
2002-01-16 17:55:41 +0100

    Devfs support on installation, and rewritten debconf scripts. Now it works at it should do

--- debian/scripts/upload_microcode.sh
+++ debian/scripts/upload_microcode.sh
@@ -5,7 +5,7 @@
 # Usage: upload_mc [microcode]
 #
 if [ "$#" -lt 1 ]; then
-   /usr/share/em8300/microcode_upload.pl /etc/dxr3.ux
+   [ -x /usr/share/em8300/microcode_upload.pl ] && /usr/share/em8300/microcode_upload.pl /etc/dxr3.ux
 else
-   /usr/share/em8300/microcode_upload.pl "$1"
+   [ -x /usr/share/em8300/microcode_upload.pl ] && /usr/share/em8300/microcode_upload.pl "$1"
 fi

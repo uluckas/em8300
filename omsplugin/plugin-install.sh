524b982b3d05f73c1e5af1b29b3b9599418cae2b
David Holm <dholm@gentoo.org>
2002-01-20 12:33:57 +0100

    Removing old files

--- omsplugin/plugin-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cp -a dxr3 $1/src/plugin/decode
-cp Makefile.am $1/src/plugin/decode
-cp Makefile.am.plugin $1/src/plugin/Makefile.am
-cp bootstrap $1
-echo "Now you are ready to build oms."

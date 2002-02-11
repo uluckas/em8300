96e573271023bba7a403b75046fbbbcc6449c038
xora <none@none>
2002-02-11 18:45:44 +0100

    depreciate old naming scheme (bah I cant spell)

--- modules/devices.sh
+++ modules/devices.sh
@@ -7,10 +7,14 @@ DEVFS=`cat /proc/mounts | grep devfs`
 
 if [ -z "$DEVFS" ] ; then
 	echo "Devfs not mounted creating device nodes"
-	mknod /dev/em8300    c 121 0
-	mknod /dev/em8300_mv c 121 1
-	mknod /dev/em8300_ma c 121 2
-	mknod /dev/em8300_sp c 121 3
+	mknod /dev/em8300-0    c 121 0
+	ln -fs /dev/em8300-0 /dev/em8300
+	mknod /dev/em8300_mv-0 c 121 1
+	ln -fs /dev/em8300_mv-0 /dev/em8300_mv
+	mknod /dev/em8300_ma-0 c 121 2
+	ln -fs /dev/em8300_ma-0 /dev/em8300_ma
+	mknod /dev/em8300_sp-0 c 121 3
+	ln -fs /dev/em8300_sp-0 /dev/em8300_sp
 	chmod 666 /dev/em8300*
 else
 	echo "Hmmm, looks like you have devfs mounted, Im going to be safe and leave things alone!"
